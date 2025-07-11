afdaf6373cfafbc67e9ee40f88e22a56f44eb222
Alan Wan <dynasty1709@gmail.com>
2025-07-10 21:55:13 -0700

    adde dmore code

--- rock.js
+++ rock.js
@@ -1,1 +1,38 @@
-console.log("hello world!")
+console.log("Lets Play Rock Paper Scissors")
+let winner = ""
+const paper = 0
+const rock = 1
+const scissors = 2
+
+
+
+function getComputerChoice(){
+num = Math.Random * 100
+if (num <20){
+    return 1;}
+else if(num >=20 && num <=50){
+    return 2;
+}
+else if(num >50){
+    return 0;
+}
+}
+
+function getPlayerChoice(){
+    console.log("Please Choose Rock Paper or Scissor")
+    console.log("Enter 0 for Paper, 1 for Rock, and 2 for Scissor")
+    return prompt("Enter Choice Now: ")
+}
+
+let compChoice = getComputerChoice()
+if (compChoice == 1){
+    console.log("Computer has chosen Rock")
+}
+if (compChoice == 0){
+    console.log("Computer has chosen Paper")
+}
+if (compChoice == 2){
+    console.log("Computer has chosen Scissor")
+}
+console.log(winner + "has won the game");
+
