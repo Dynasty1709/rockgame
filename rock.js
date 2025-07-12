6ad8ecaf9e962b8501d10da792a66e28a8bd064f
Alan Wan <dynasty1709@gmail.com>
2025-07-12 16:04:50 -0700

    aded

--- rock.js
+++ rock.js
@@ -1,10 +1,19 @@
 console.log("Lets Play Rock Paper Scissors")
+console.log("Please Choose Rock Paper or Scissor")
+console.log("Enter 0 for Paper, 1 for Rock, and 2 for Scissor")
 let winner = ""
 const paper = 0
 const rock = 1
 const scissors = 2
 let playerScore = 0
 let computerScore = 0;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        let playerChoice = button.id
+    });
+});
+
 
 function getComputerChoice(){
 num = Math.Random * 100
@@ -18,15 +27,10 @@ else{
 }
 }
 
-function getPlayerChoice(){
-    console.log("Please Choose Rock Paper or Scissor")
-    console.log("Enter 0 for Paper, 1 for Rock, and 2 for Scissor")
-    return prompt("Enter Choice Now: ")
-}
 let rounds = 0;
-while(rounds <=5){
+
 let compChoice = getComputerChoice()
-let playerChoice = getPlayerChoice()
+
 
 if (playerChoice == 1){
     console.log("Player has chosen Rock")
@@ -81,5 +85,6 @@ computerScore +=1}
 rounds +=1
 console.log(winner + " has won the game");
 console.log("Current Score: " + "Computer: "+computerScore +" Player: "+ playerScore)
-}
+
+
 
